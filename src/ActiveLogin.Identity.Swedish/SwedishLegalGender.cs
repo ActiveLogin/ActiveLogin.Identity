1c472bfb451cca7fd76ca3fce84050ead70787c5
Peter Örneholm <peter@orneholm.com>
2018-07-13 15:57:58 +0200

    Add documentation for SwedishLegalGender

--- src/ActiveLogin.Identity.Swedish/SwedishLegalGender.cs
+++ src/ActiveLogin.Identity.Swedish/SwedishLegalGender.cs
@@ -1,10 +1,23 @@
 ﻿namespace ActiveLogin.Identity.Swedish
 {
+    /// <summary>
+    /// Specifies possible legal genders (juridiskt kön) in sweden.
+    /// </summary>
     public enum SwedishLegalGender
     {
+        /// <summary>
+        /// Unknown or other gender
+        /// </summary>
         Other,
 
+        /// <summary>
+        /// Gender representing a woman
+        /// </summary>
         Woman,
+
+        /// <summary>
+        /// Gender representing a man
+        /// </summary>
         Man
     }
 }
