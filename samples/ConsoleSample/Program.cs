b0e26d71f3ac97a34f926e626c0d706621d9ee4d
Peter Örneholm <peter@orneholm.com>
2018-07-23 09:30:18 +0200

    Add console sample for SwedishPersonalIdentityNumber

---
+++ samples/ConsoleSample/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using ActiveLogin.Identity.Swedish;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        private static readonly string[] RawPersonalIdentityNumberSamples =
+        {
+            "990913+9801",
+            "120211+9986",
+            "990807-2391",
+            "180101-2392",
+
+            "180101.2392",
+            "ABC",
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample showing possible uses of SwedishPersonalIdentityNumber.");
+            WriteSpace();
+
+            foreach (var sample in RawPersonalIdentityNumberSamples)
+            {
+                WritePersonalIdentityNumberInfo(sample);
+                WriteSpace();
+            }
+
+            WriteSpace();
+            Console.WriteLine("What is your (Swedish) Personal Identity Number?");
+            var userRawPersonalIdentityNumber = Console.ReadLine();
+            WritePersonalIdentityNumberInfo(userRawPersonalIdentityNumber);
+            WriteSpace();
+
+            Console.ReadLine();
+        }
+
+        private static void WritePersonalIdentityNumberInfo(string rawPersonalIdentityNumber)
+        {
+            WriteHeader($"Input: {rawPersonalIdentityNumber}");
+            if (SwedishPersonalIdentityNumber.TryParse(rawPersonalIdentityNumber, out var personalIdentityNumber))
+            {
+                WritePersonalIdentityNumberInfo(personalIdentityNumber);
+            }
+            else
+            {
+                Console.Error.WriteLine("Unable to parse the input as a SwedishPersonalIdentityNumber.");
+            }
+        }
+
+        private static void WritePersonalIdentityNumberInfo(SwedishPersonalIdentityNumber personalIdentityNumber)
+        {
+            Console.WriteLine("SwedishPersonalIdentityNumber");
+            WriteKeyValueInfo("   .ToString()", personalIdentityNumber.ToString());
+            WriteKeyValueInfo("   .ToShortString()", personalIdentityNumber.ToShortString());
+            WriteKeyValueInfo("   .ToLongString()", personalIdentityNumber.ToLongString());
+
+            WriteKeyValueInfo("   .Year", personalIdentityNumber.Year.ToString());
+            WriteKeyValueInfo("   .Month", personalIdentityNumber.Month.ToString());
+            WriteKeyValueInfo("   .Day", personalIdentityNumber.Day.ToString());
+            WriteKeyValueInfo("   .SerialNumber", personalIdentityNumber.SerialNumber.ToString());
+            WriteKeyValueInfo("   .Checksum", personalIdentityNumber.Checksum.ToString());
+
+            WriteKeyValueInfo("   .DateOfBirth", personalIdentityNumber.DateOfBirth.ToShortDateString());
+            WriteKeyValueInfo("   .GetAge()", personalIdentityNumber.GetAge().ToString());
+
+            WriteKeyValueInfo("   .Gender", personalIdentityNumber.Gender.ToString());
+        }
+
+        private static void WriteHeader(string header)
+        {
+            Console.WriteLine(header);
+            Console.WriteLine("----------------------");
+        }
+
+        private static void WriteKeyValueInfo(string key, string value)
+        {
+            Console.WriteLine($"{key}: {value}");
+        }
+
+        private static void WriteSpace()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
