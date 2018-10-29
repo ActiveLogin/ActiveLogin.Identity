af270e0433ff83db755704cadc2bb388ab8ce5a5
Peter Örneholm <peter@orneholm.com>
2018-10-29 08:12:39 +0100

    Rename serialNumber into birthNumber (#34)

--- test/ActiveLogin.Identity.Swedish.Test/SwedishPersonalIdentityNumberHintExtensions_GetDateOfBirthHint.cs
+++ test/ActiveLogin.Identity.Swedish.Test/SwedishPersonalIdentityNumberHintExtensions_GetDateOfBirthHint.cs
@@ -12,9 +12,9 @@ namespace ActiveLogin.Identity.Swedish.Test
         [Theory]
         [InlineData(1899, 09, 13, 980, 1)]
         [InlineData(1912, 02, 11, 998, 6)]
-        public void Year_Equals_Year(int year, int month, int day, int serialNumber, int checksum)
+        public void Year_Equals_Year(int year, int month, int day, int birthNumber, int checksum)
         {
-            var personalIdentityNumber = SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum);
+            var personalIdentityNumber = SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum);
             var dateOfBirth = personalIdentityNumber.GetDateOfBirthHint();
             Assert.Equal(year, dateOfBirth.Year);
         }
@@ -22,9 +22,9 @@ namespace ActiveLogin.Identity.Swedish.Test
         [Theory]
         [InlineData(1899, 09, 13, 980, 1)]
         [InlineData(1912, 02, 11, 998, 6)]
-        public void Month_Equals_Month(int year, int month, int day, int serialNumber, int checksum)
+        public void Month_Equals_Month(int year, int month, int day, int birthNumber, int checksum)
         {
-            var personalIdentityNumber = SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum);
+            var personalIdentityNumber = SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum);
             var dateOfBirth = personalIdentityNumber.GetDateOfBirthHint();
             Assert.Equal(month, dateOfBirth.Month);
         }
@@ -32,9 +32,9 @@ namespace ActiveLogin.Identity.Swedish.Test
         [Theory]
         [InlineData(1899, 09, 13, 980, 1)]
         [InlineData(1912, 02, 11, 998, 6)]
-        public void Day_Equals_Day(int year, int month, int day, int serialNumber, int checksum)
+        public void Day_Equals_Day(int year, int month, int day, int birthNumber, int checksum)
         {
-            var personalIdentityNumber = SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum);
+            var personalIdentityNumber = SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum);
             var dateOfBirth = personalIdentityNumber.GetDateOfBirthHint();
             Assert.Equal(day, dateOfBirth.Day);
         }
--- test/ActiveLogin.Identity.Swedish.Test/SwedishPersonalIdentityNumber_Create.cs
+++ test/ActiveLogin.Identity.Swedish.Test/SwedishPersonalIdentityNumber_Create.cs
@@ -12,18 +12,18 @@ namespace ActiveLogin.Identity.Swedish.Test
         [Theory]
         [InlineData(-1, 01, 01, 239, 2)]
         [InlineData(int.MaxValue, 01, 01, 239, 2)]
-        public void Throws_When_Invalid_Year(int year, int month, int day, int serialNumber, int checksum)
+        public void Throws_When_Invalid_Year(int year, int month, int day, int birthNumber, int checksum)
         {
-            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum));
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum));
             Assert.Contains("Invalid year.", ex.Message);
         }
 
         [Theory]
         [InlineData(2018, 0, 01, 239, 2)]
         [InlineData(2018, 13, 01, 239, 2)]
-        public void Throws_When_Invalid_Month(int year, int month, int day, int serialNumber, int checksum)
+        public void Throws_When_Invalid_Month(int year, int month, int day, int birthNumber, int checksum)
         {
-            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum));
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum));
             Assert.Contains("Invalid month.", ex.Message);
         }
 
@@ -31,35 +31,35 @@ namespace ActiveLogin.Identity.Swedish.Test
         [InlineData(2018, 01, 0, 239, 2)]
         [InlineData(2018, 01, 32, 239, 2)]
         [InlineData(2018, 02, 30, 239, 2)]
-        public void Throws_When_Invalid_Day(int year, int month, int day, int serialNumber, int checksum)
+        public void Throws_When_Invalid_Day(int year, int month, int day, int birthNumber, int checksum)
         {
-            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum));
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum));
             Assert.Contains("Invalid day of month.", ex.Message);
         }
 
         [Theory]
         [InlineData(2018, 01, 61, 239, 2)]
-        public void Throws_When_Possible_CoOrdinationNumber(int year, int month, int day, int serialNumber, int checksum)
+        public void Throws_When_Possible_CoOrdinationNumber(int year, int month, int day, int birthNumber, int checksum)
         {
-            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum));
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum));
             Assert.Contains("Invalid day of month.", ex.Message);
         }
 
         [Theory]
         [InlineData(2018, 01, 01, 0, 2)]
         [InlineData(2018, 01, 01, 1000, 2)]
-        public void Throws_When_Invalid_SerialNumber(int year, int month, int day, int serialNumber, int checksum)
+        public void Throws_When_Invalid_BirthNumber(int year, int month, int day, int birthNumber, int checksum)
         {
-            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum));
-            Assert.Contains("Invalid serial number.", ex.Message);
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum));
+            Assert.Contains("Invalid birth number.", ex.Message);
         }
 
         [Theory]
         [InlineData(2018, 01, 01, 239, 3)]
         [InlineData(2018, 01, 01, 239, 4)]
-        public void Throws_When_Invalid_Checksum(int year, int month, int day, int serialNumber, int checksum)
+        public void Throws_When_Invalid_Checksum(int year, int month, int day, int birthNumber, int checksum)
         {
-            var ex = Assert.Throws<ArgumentException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum));
+            var ex = Assert.Throws<ArgumentException>(() => SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum));
             Assert.Contains("Invalid checksum.", ex.Message);
         }
 
@@ -68,13 +68,13 @@ namespace ActiveLogin.Identity.Swedish.Test
         [InlineData(1999, 08, 07, 239, 1)]
         [InlineData(2000, 01, 02, 239, 1)]
         [InlineData(2018, 01, 01, 239, 2)]
-        public void Accepts_Valid_Personal_Identity_Number(int year, int month, int day, int serialNumber, int checksum)
+        public void Accepts_Valid_Personal_Identity_Number(int year, int month, int day, int birthNumber, int checksum)
         {
-            var personalIdentityNumber = SwedishPersonalIdentityNumber.Create(year, month, day, serialNumber, checksum);
+            var personalIdentityNumber = SwedishPersonalIdentityNumber.Create(year, month, day, birthNumber, checksum);
             Assert.Equal(year, personalIdentityNumber.Year);
             Assert.Equal(month, personalIdentityNumber.Month);
             Assert.Equal(day, personalIdentityNumber.Day);
-            Assert.Equal(serialNumber, personalIdentityNumber.SerialNumber);
+            Assert.Equal(birthNumber, personalIdentityNumber.BirthNumber);
             Assert.Equal(checksum, personalIdentityNumber.Checksum);
         }
     }
