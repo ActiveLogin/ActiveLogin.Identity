69dc924115ba34f40567a4c8f3cc8e006fc17893
Peter Örneholm <peter@orneholm.com>
2018-10-31 19:34:55 +0100

    Rename date into currentYear

--- src/ActiveLogin.Identity.Swedish/SwedishPersonalIdentityNumber.cs
+++ src/ActiveLogin.Identity.Swedish/SwedishPersonalIdentityNumber.cs
@@ -108,12 +108,12 @@ namespace ActiveLogin.Identity.Swedish
         /// Converts the string representation of the personal identity number to its <see cref="SwedishPersonalIdentityNumber"/> equivalent.
         /// </summary>
         /// <param name="personalIdentityNumber">A string representation of the personal identity number to parse.</param>
-        /// <param name="date">The date to decide whether the person is has turned / will turn 100 years old that year. That decides the delimiter (- or +).</param>
-        public static SwedishPersonalIdentityNumber Parse(string personalIdentityNumber, DateTime date)
+        /// <param name="currentYear">The date to decide whether the person is has turned / will turn 100 years old that year. That decides the delimiter (- or +).</param>
+        public static SwedishPersonalIdentityNumber Parse(string personalIdentityNumber, DateTime currentYear)
         {
             try
             {
-                var parsed = SwedishPersonalIdentityNumberParser.Parse(personalIdentityNumber, date);
+                var parsed = SwedishPersonalIdentityNumberParser.Parse(personalIdentityNumber, currentYear);
                 return new SwedishPersonalIdentityNumber(parsed.Year, parsed.Month, parsed.Day, parsed.BirthNumber, parsed.Checksum);
             }
             catch (ArgumentException)
@@ -145,13 +145,13 @@ namespace ActiveLogin.Identity.Swedish
         /// Converts the string representation of the personal identity number to its <see cref="SwedishPersonalIdentityNumber"/> equivalent  and returns a value that indicates whether the conversion succeeded.
         /// </summary>
         /// <param name="personalIdentityNumber">A string representation of the personal identity number to parse.</param>
-        /// <param name="date">The date to decide whether the person is has turned / will turn 100 years old that year. That decides the delimiter (- or +).</param>
+        /// <param name="currentYear">The date to decide whether the person is has turned / will turn 100 years old that year. That decides the delimiter (- or +).</param>
         /// <param name="result">If valid, an instance of <see cref="SwedishPersonalIdentityNumber"/></param>
-        public static bool TryParse(string personalIdentityNumber, DateTime date, out SwedishPersonalIdentityNumber result)
+        public static bool TryParse(string personalIdentityNumber, DateTime currentYear, out SwedishPersonalIdentityNumber result)
         {
             try
             {
-                result = Parse(personalIdentityNumber, date);
+                result = Parse(personalIdentityNumber, currentYear);
                 return true;
             }
             catch (Exception)
@@ -174,11 +174,11 @@ namespace ActiveLogin.Identity.Swedish
         /// Converts the value of the current <see cref="SwedishPersonalIdentityNumber" /> object to its equivalent 10 digit string representation. The total length, including the separator, will be 11 chars.
         /// Format is YYMMDDXSSSC, for example <example>990807-2391</example> or <example>120211+9986</example>.
         /// </summary>
-        /// <param name="date">The date to decide whether the person is has turned / will turn 100 years old that year. That decides the delimiter (- or +).</param>
+        /// <param name="currentYear">The date to decide whether the person is has turned / will turn 100 years old that year. That decides the delimiter (- or +).</param>
 
-        public string To10DigitString(DateTime date)
+        public string To10DigitString(DateTime currentYear)
         {
-            var years = date.Year - Year;
+            var years = currentYear.Year - Year;
             var delimiter = years >= 100 ? '+' : '-';
             var twoDigitYear = Year % 100;
             return $"{twoDigitYear:D2}{Month:D2}{Day:D2}{delimiter}{BirthNumber:D3}{Checksum}";
--- src/ActiveLogin.Identity.Swedish/SwedishPersonalIdentityNumberParser.cs
+++ src/ActiveLogin.Identity.Swedish/SwedishPersonalIdentityNumberParser.cs
@@ -5,11 +5,11 @@ namespace ActiveLogin.Identity.Swedish
 {
     internal static class SwedishPersonalIdentityNumberParser
     {
-        public static SwedishPersonalIdentityNumberParts Parse(string personalIdentityNumber, DateTime date)
+        public static SwedishPersonalIdentityNumberParts Parse(string personalIdentityNumber, DateTime currentYear)
         {
             var trimmedPersonalIdentityNumber = personalIdentityNumber.Trim();
 
-            if (TryParseShortPattern(trimmedPersonalIdentityNumber, date, out var parsedShort))
+            if (TryParseShortPattern(trimmedPersonalIdentityNumber, currentYear, out var parsedShort))
             {
                 return parsedShort;
             }
@@ -25,7 +25,7 @@ namespace ActiveLogin.Identity.Swedish
         /// <summary>
         /// YYYYMMDDSSSC, YYYYMMDD-SSSC or YYYYMMDD+SSSC
         /// </summary>
-        private static bool TryParseShortPattern(string personalIdentityNumber, DateTime date, out SwedishPersonalIdentityNumberParts parts)
+        private static bool TryParseShortPattern(string personalIdentityNumber, DateTime currentYear, out SwedishPersonalIdentityNumberParts parts)
         {
             var pattern = new Regex(@"^" +
                             @"(?<year>[0-9]{2})" +
@@ -45,7 +45,7 @@ namespace ActiveLogin.Identity.Swedish
 
             var partsFromMatch = GetPartsFromMatch(match);
             var delimiter = GetStringValue(match, "delimiter");
-            var fullYear = GetFullYear(partsFromMatch.Year, partsFromMatch.Month, partsFromMatch.Day, delimiter, date);
+            var fullYear = GetFullYear(partsFromMatch.Year, partsFromMatch.Month, partsFromMatch.Day, delimiter, currentYear);
         
             parts = new SwedishPersonalIdentityNumberParts(fullYear, partsFromMatch.Month, partsFromMatch.Day, partsFromMatch.BirthNumber, partsFromMatch.Checksum);
             return true;
@@ -97,13 +97,13 @@ namespace ActiveLogin.Identity.Swedish
             return longPatternMatch.Groups[groupName].Value;
         }
 
-        private static int GetFullYear(int shortYear, int month, int day, string delimiter, DateTime date)
+        private static int GetFullYear(int shortYear, int month, int day, string delimiter, DateTime currentYear)
         {
-            var currentCentury = (date.Year / 100) * 100;
+            var currentCentury = (currentYear.Year / 100) * 100;
             var fullYear = currentCentury + shortYear;
 
             var dateOfBirth = new DateTime(fullYear, month, day, 0, 0, 0, DateTimeKind.Utc);
-            if (dateOfBirth.Year > date.Year)
+            if (dateOfBirth.Year > currentYear.Year)
             {
                 dateOfBirth = dateOfBirth.AddYears(-100);
             }
