7f5c349683958c9377ebdf2c5de5810f3371ffe9
Viktor Andersson <v.andersson@me.com>
2018-12-18 22:28:10 +0100

    Formatting (#54)
    * Fix formatting for ConsoleRunner.
    
    * Fix formatting for regex

--- samples/ConsoleSample.FSharp/Program.fs
+++ samples/ConsoleSample.FSharp/Program.fs
@@ -1,35 +1,36 @@
 ﻿module ConsoleSample.FSharp
 
-open System
 open ActiveLogin.Identity.Swedish.FSharp
+open System
 
-let sampleStrings =
-    [ "990913+9801"
-      "120211+9986"
-      "990807-2391"
-      "180101-2392"
-      "180101.2392"
-      "ABC" ]
+let sampleStrings = [ "990913+9801"; "120211+9986"; "990807-2391"; "180101-2392"; "180101.2392"; "ABC" ]
 
 let parseAndPrintPersonalIdentityNumber str =
-    let printHeader str = str |> printfn "Input: %s\n----------------------"
-    let print10DigitString pin = 
-        pin 
-        |> SwedishPersonalIdentityNumber.to10DigitString 
-        |> printfn "SwedishPersonalIdentityNumber.to10DigitString: %s" 
+    let printHeader str =
+        str
+        |> printfn "Input: %s\n----------------------"
+
+    let print10DigitString pin =
+        pin
+        |> SwedishPersonalIdentityNumber.to10DigitString
+        |> printfn "SwedishPersonalIdentityNumber.to10DigitString: %s"
+
     let print12DigitString pin =
-        pin 
-        |> SwedishPersonalIdentityNumber.to12DigitString 
-        |> printfn "SwedishPersonalIdentityNumber.to12DigitString: %s" 
+        pin
+        |> SwedishPersonalIdentityNumber.to12DigitString
+        |> printfn "SwedishPersonalIdentityNumber.to12DigitString: %s"
+
     let printDateOfBirthHint pin =
-        let date = pin |> SwedishPersonalIdentityNumber.Hints.getDateOfBirthHint 
+        let date = pin |> SwedishPersonalIdentityNumber.Hints.getDateOfBirthHint
         date.ToShortDateString() |> printfn "SwedishPersonalIdentityNumber.Hints.getDateOfBirthHint: %s"
+
     let printAgeHint pin =
-        pin 
-        |> SwedishPersonalIdentityNumber.Hints.getAgeHintOnDate DateTime.UtcNow 
+        pin
+        |> SwedishPersonalIdentityNumber.Hints.getAgeHintOnDate DateTime.UtcNow
         |> printfn "SwedishPersonalIdentityNumber.Hints.getAgeHintOnDate: %i"
+
     let printGenderHint pin =
-        let gender = pin |> SwedishPersonalIdentityNumber.Hints.getGenderHint 
+        let gender = pin |> SwedishPersonalIdentityNumber.Hints.getGenderHint
         gender.ToString() |> printfn "SwedishPersonalIdentityNumber.Hints.getGenderHint: %s"
 
     printHeader str
@@ -43,21 +44,15 @@ let parseAndPrintPersonalIdentityNumber str =
         printAgeHint pin
         printGenderHint pin
     | Error e -> printfn "%A: Unable to parse the input as a SwedishPersonalIdentityNumber." e
-    
     printf "\n\n"
 
 [<EntryPoint>]
 let main argv =
     printfn "Sample showing possible uses of SwedishPersonalIdentityNumber."
     printf "\n\n"
-
-    sampleStrings
-    |> List.iter parseAndPrintPersonalIdentityNumber
-
+    sampleStrings |> List.iter parseAndPrintPersonalIdentityNumber
     printfn "What is your (Swedish) Personal Identity Number?"
     let userInput = Console.ReadLine()
     parseAndPrintPersonalIdentityNumber userInput
-
     Console.ReadLine() |> ignore
-
     0 // return an integer exit code
