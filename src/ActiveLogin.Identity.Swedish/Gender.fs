b5c18bda54ffbcb3943054cc785fd66ecd3b6b45
Peter Örneholm <peter@orneholm.com>
2018-12-18 16:02:57 +0100

    Fix swedish

--- src/ActiveLogin.Identity.Swedish/Gender.fs
+++ src/ActiveLogin.Identity.Swedish/Gender.fs
@@ -1,7 +1,7 @@
 namespace ActiveLogin.Identity.Swedish
 
 /// <summary>
-/// Specifies possible genders in Sweden. 
+/// Specifies possible genders (juridiskt kön) in Sweden.
 /// </summary>
 type Gender =
     /// <summary>
