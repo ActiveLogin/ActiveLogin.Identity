35fad356af909f8011249b8cb9c6b4a627d79b24
Viktor Andersson <v.andersson@me.com>
2019-03-16 15:16:04 +0100

    Formatting

--- test/ActiveLogin.Identity.Swedish.FSharp.Test/SwedishPersonalIdentityNumber_create.fs
+++ test/ActiveLogin.Identity.Swedish.FSharp.Test/SwedishPersonalIdentityNumber_create.fs
@@ -22,22 +22,28 @@ let tests = testList "create" [
     testProp [ typeof<ValidValuesGen> ] "valid pin" <| fun values ->
         let result = values |> SwedishPersonalIdentityNumber.create
         result |> Expect.equalPin values
+
     testProp [ typeof<InvalidYearGen> ] "Invalid year" <| fun values ->
         let result = values |> SwedishPersonalIdentityNumber.create
         result =! Error(InvalidYear values.Year)
+
     testProp [ typeof<InvalidMonthGen> ] "Invalid month" <| fun values ->
         let result = values |> SwedishPersonalIdentityNumber.create
         result =! Error(InvalidMonth values.Month)
+
     testProp [ typeof<InvalidDayGen> ] "Invalid day" <| fun values ->
         let result = values |> SwedishPersonalIdentityNumber.create
         result =! Error(InvalidDayAndCoordinationDay values.Day)
+
     testProp [ typeof<ValidValuesGen> ] "Possible coordination-number day" <| fun values ->
         let coordinationDay = values.Day + 60
         let result = { values with Day = coordinationDay } |> SwedishPersonalIdentityNumber.create
         result =! Error(InvalidDay coordinationDay)
+
     testProp [ typeof<InvalidBirthNumberGen> ] "Invalid birthnumber" <| fun values ->
         let result = values |> SwedishPersonalIdentityNumber.create
         result =! Error(InvalidBirthNumber values.BirthNumber)
+
     testProp [ typeof<ValidValuesGen> ]
         "With all other values valid only 1 out of 10 checksums is valid" <| fun values ->
         let isValid x =
@@ -57,6 +63,7 @@ let tests = testList "create" [
             |> List.filter isValid
             |> List.length
         numValid =! 1
+
     testCase "FSharp should have no public constructor" <| fun () ->
         let typ = typeof<SwedishPersonalIdentityNumber>
         let numConstructors = typ.GetConstructors(BindingFlags.Public) |> Array.length
