f4365df26ccf32687a5f09d28067f55b14867285
Viktor Andersson <v.andersson@me.com>
2019-11-04 07:41:00 +0100

    Remove doc-comments from internal module

--- src/ActiveLogin.Identity.Swedish/StringHelpers.fs
+++ src/ActiveLogin.Identity.Swedish/StringHelpers.fs
@@ -23,16 +23,6 @@ let private parseDay day =
     | CoordinationDay cd -> cd.Value
 
 
-/// <summary>
-/// Converts a SwedishPersonalIdentityNumber to its equivalent 10 digit string representation. The total length, including the separator, will be 11 chars.
-/// </summary>
-/// <param name="serializationYear">
-/// The specific year to use when checking if the person has turned / will turn 100 years old.
-/// That information changes the delimiter (- or +).
-///
-/// For more info, see: https://www.riksdagen.se/sv/dokument-lagar/dokument/svensk-forfattningssamling/folkbokforingslag-1991481_sfs-1991-481#P18
-/// </param>
-/// <param name="pin">A SwedishPersonalIdentityNumber</param>
 let to10DigitStringInSpecificYear serializationYear (num : IdentityNumber) =
     result {
         let! validYear = validSerializationYear serializationYear num.Year
@@ -49,10 +39,6 @@ let to10DigitStringInSpecificYear serializationYear (num : IdentityNumber) =
             num.Checksum.Value
     }
 
-/// <summary>
-/// Converts a SwedishPersonalIdentityNumber to its equivalent 10 digit string representation. The total length, including the separator, will be 11 chars.
-/// </summary>
-/// <param name="pin">A SwedishPersonalIdentityNumber</param>
 let to10DigitString (num : IdentityNumber) =
     let year =
         DateTime.UtcNow.Year
@@ -62,11 +48,6 @@ let to10DigitString (num : IdentityNumber) =
         | Error _ -> invalidArg "year" "DateTime.Year wasn't a valid year"
     to10DigitStringInSpecificYear year num
 
-/// <summary>
-/// Converts the value of the current <see cref="SwedishPersonalIdentityNumber" /> object to its equivalent 12 digit string representation.
-/// Format is YYYYMMDDBBBC, for example <example>19908072391</example> or <example>191202119986</example>.
-/// </summary>
-/// <param name="pin">A SwedishPersonalIdentityNumber</param>
 let to12DigitString (num: IdentityNumber) =
     sprintf "%02i%02i%02i%03i%1i"
         num.Year.Value
