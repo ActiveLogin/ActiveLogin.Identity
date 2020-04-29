68a07acdc97cf4f591c7b6b8ca14a993f0c93a16
Viktor Andersson <v.andersson@me.com>
2020-04-29 09:57:11 +0200

    Add sample to verify TestData extensions.

---
+++ samples/ExtensionTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ActiveLogin.Identity.Swedish;
+
+namespace ExtensionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "120211+9986";
+            var pin = SwedishPersonalIdentityNumber.Parse(input);
+            Console.WriteLine($"{input} is a test number: {pin.IsTestNumber()}");
+        }
+    }
+}
