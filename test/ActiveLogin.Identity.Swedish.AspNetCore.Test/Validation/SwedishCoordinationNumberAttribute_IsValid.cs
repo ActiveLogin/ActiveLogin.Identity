cde2d7751383817e07768f68c8ca97ca4b344997
Tor Seldén <tor.selden@nethouse.se>
2023-08-24 08:10:41 +0200

    Update test data and source link

--- test/ActiveLogin.Identity.Swedish.AspNetCore.Test/Validation/SwedishCoordinationNumberAttribute_IsValid.cs
+++ test/ActiveLogin.Identity.Swedish.AspNetCore.Test/Validation/SwedishCoordinationNumberAttribute_IsValid.cs
@@ -7,14 +7,14 @@ namespace ActiveLogin.Identity.Swedish.AspNetCore.Test.Validation
 {
     /// <remarks>
     /// Tested with official test Coordination Numbers from Skatteverket:
-    /// https://skatteverket.entryscape.net/store/9/resource/1887
+    /// https://swedish.identityinfo.net/coordinationnumber/testdata
     /// </remarks>
     public class SwedishCoordinationNumberAttribute_IsValid
     {
         [Fact]
         public void Returns_Valid_When_Valid_Coordination_Number()
         {
-            var model = new SampleValidationModel("230161-2392");
+            var model = new SampleValidationModel("480977-2389");
             var context = new ValidationContext(model);
             var results = new List<ValidationResult>();
             var isValid = Validator.TryValidateObject(model, context, results, true);
